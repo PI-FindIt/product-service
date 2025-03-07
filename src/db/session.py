e71bfc47e2daa4311d1061cc3ff120308331d951
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-03-07 02:03:41 +0000

    Configure async databases

--- src/db/session.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import create_engine, Session
-
-# Pode ser MongoDB, ou PostgreSQL
-DATABASE_URL = "TODO"
-engine = create_engine(DATABASE_URL, echo=True)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
