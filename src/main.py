ca9fe77175a1f51883a053c74bbac11de61f30fa
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-03-07 17:45:12 +0000

    Add strawberry

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
+import threading
+
+import strawberry
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
 from src.api.routes import router
 from src.api.service import serve_grpc
-import threading
 
 grpc_thread = threading.Thread(target=serve_grpc)
 grpc_thread.start()
 
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return f"Hello World!"
+
+
+schema = strawberry.Schema(query=Query)
+graphql_app = GraphQLRouter(schema)
+
+
 app = FastAPI(title="Microservice Template")
 app.include_router(router)
+app.include_router(graphql_app, prefix="/graphql")
