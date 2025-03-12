b28d76a3842de9a39f14fa60d1b56d21a1ab4411
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-03-12 23:42:39 +0000

    Add Mutation to main template

--- templates/main.template.py
+++ templates/main.template.py
@@ -6,7 +6,7 @@ import strawberry
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 
-from src.api.graphql import Query
+from src.api.graphql import Query, Mutation
 from src.api.routes import router
 from src.api.service import serve_grpc
 from src.config.session import init_postgres_db
@@ -19,7 +19,7 @@ async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
     yield
 
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 graphql_app = GraphQLRouter(schema)
 
 app = FastAPI(title="ServiceName", lifespan=lifespan)
