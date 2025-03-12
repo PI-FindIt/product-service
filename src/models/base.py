6fbe4622e0e209db424bd890f6eb5e9e04496645
andre <andrepedro2004@hotmail.com>
2025-03-12 13:15:15 +0000

    giving up

--- src/models/base.py
+++ src/models/base.py
@@ -1,36 +1,50 @@
-from typing import Type, ClassVar, Self, Any
+from typing import Type, Self, Any, TypeVar, ClassVar
 
 import strawberry
 from google.protobuf.json_format import ParseDict, MessageToDict
 from google.protobuf.message import Message
 from sqlmodel import SQLModel
-from strawberry.experimental.pydantic.conversion_types import StrawberryTypeFromPydantic
 
 
-class BaseModel(SQLModel):
+T = TypeVar("T", bound=Message)
+
+
+class BaseModel[T](SQLModel):
     """A superclass that acts as SQLModel, Strawberry GraphQL type and converts to gRPC Messages."""
 
-    __grpc_model__ = Message
-    __strawberry_type__ = None
+    __grpc_model__: Type[T]
 
-    def __init_subclass__(cls, **kwargs: Any) -> None:
-        super().__init_subclass__()
-        cls.__strawberry_type__ = cls.as_strawberry_type()
+    __strawberry_model__ = None
+    # __strawberry_type__: ClassVar[type] = None
 
-    @classmethod
-    def as_strawberry_type(cls) -> Type[StrawberryTypeFromPydantic[Self]]:
-        @strawberry.experimental.pydantic.type(model=cls, all_fields=True)
-        class _StrawberryModel:
-            pass
+    # def __init_subclass__(cls, **kwargs: dict[str, Any]) -> None:
+    #     super().__init_subclass__(**kwargs)
+    #
+    #     @strawberry.experimental.pydantic.type(model=cls, all_fields=True)
+    #     class _StrawberryType:
+    #         pass
+    #
+    #     cls.__strawberry_model__ = _StrawberryType
+    #     cls.__strawberry_type__ = strawberry.type(
+    #         cls.__strawberry_model__, name=cls.__name__
+    #     )
+    #
+    # def to_strawberry(self) -> StrawberryTypeFromPydantic[Self]:
+    #     """Convert SQLModel instance to Strawberry type."""
+    #     return self.__strawberry_model__.from_pydantic(self)
 
-        return _StrawberryModel
-
-    def to_grpc(self) -> __grpc_model__:
+    def to_grpc(self) -> T:
         """Convert SQLModel instance to gRPC message."""
-        return ParseDict(self.model_dump(), self.__grpc_model__())
+        return self.__grpc_model__(**self.model_dump())
+
+    # def to_grpc(self) -> T:
+    #     """Convert SQLModel instance to gRPC message, ensuring no None values."""
+    #     data = {k: v if v is not None else "" for k, v in self.model_dump().items() if
+    #             k in self.__grpc_model__.__annotations__}
+    #     return self.__grpc_model__(**data)
 
     @classmethod
-    def from_grpc(cls, grpc_message: __grpc_model__) -> "HybridModel":
+    def from_grpc(cls, grpc_message: Message) -> Self:  # misc
         """Convert gRPC message to SQLModel instance."""
         data_dict = MessageToDict(grpc_message, preserving_proto_field_name=True)
         return cls(**data_dict)
@@ -40,6 +54,6 @@ class BaseModel(SQLModel):
         return self.model_dump()
 
     @classmethod
-    def from_dict(cls, data: dict[str, Any]) -> "HybridModel":
+    def from_dict(cls, data: dict[str, Any]) -> Self:
         """Create an instance from a dictionary."""
         return cls(**data)
