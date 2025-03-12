721461af34fe312bd9a94ff0eb0262c0f6ce20a3
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-03-12 23:46:36 +0000

    Oof

--- templates/model.template.py
+++ templates/model.template.py
@@ -5,15 +5,12 @@ from pydantic import BaseModel as PydanticBaseModel
 from sqlmodel import Field
 
 from protobuf.connections import service_name_models
-from src.crud.base import CRUDBase
 from src.models.base import BaseModel
 
 
 @strawberry.type
 class _ModelAttr(PydanticBaseModel):
-    title: str
-    author: str
-    year: int
+    example: str
 
 
 @strawberry.type
@@ -26,8 +23,3 @@ class Model(BaseModel[service_name_models.Model], _ModelAttr, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
     __grpc_model__ = service_name_models.Model
-
-
-class CrudModel(CRUDBase[Model, ModelBase, int]):
-    def __init__(self) -> None:
-        super().__init__(model=Model)
