3f06385bd882095539bf5ff07f180925634fcd0d
andre <andrepedro2004@hotmail.com>
2025-03-15 19:24:44 +0000

    making the make

---
+++ src/api/graphql.py
@@ -0,0 +1,56 @@
+from typing import Callable, Awaitable, Type, TypeVar
+
+import strawberry
+from google.protobuf.message import Message
+from grpc.aio import AioRpcError  # type: ignore
+from src.models.model import Model, ModelBase
+
+from protobuf.connections import (
+    product_service_stub,
+    product_service_models,
+)
+from src.models.base import BaseModel
+
+ModelType = TypeVar("ModelType", bound=BaseModel)  # type: ignore
+
+
+async def make_grpc_call(
+    stub_method: Callable[[Message], Awaitable[Message]],
+    response_model_cls: Type[ModelType],
+    *request_message: Message,
+) -> ModelType:
+    """
+    Make a gRPC call and return the response.
+
+    :param stub_method: The gRPC method to call (e.g., ``GetModel``).
+    :param response_model_cls: The response SQLModel class (e.g., ``Model``).
+    :param request_message: The request message to send (e.g., ``product_service_models.ModelId(id=id)``).
+    :return: The response SQLModel instance.
+    """
+    try:
+        response = await stub_method(*request_message)
+        return response_model_cls.from_grpc(response)
+    except AioRpcError as e:
+        raise Exception(e.details())
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    async def get_model(self, id: int) -> Model:
+        return await make_grpc_call(
+            product_service_stub.GetModel,
+            Model,
+            product_service_models.ModelId(id=id),
+        )
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    async def create_model(self, model: ModelBase) -> Model:
+        return await make_grpc_call(
+            product_service_stub.CreateModel,
+            Model,
+            model.to_grpc(),
+        )
