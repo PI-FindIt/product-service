b40ea693a385c44146ef65148ab9330c0e78feb7
VioletaR <violetabatistaramos@gmail.com>
2025-03-19 12:51:42 +0000

    updates in models (product and catergory)

--- src/models/model.py
+++ src/models/model.py
@@ -6,20 +6,65 @@ from sqlmodel import Field
 
 from protobuf.connections import product_service_models
 from src.models.base import BaseModel
+from enum import Enum
 
+class NutriScore(Enum):
+    A = "A"
+    B = "B"
+    C = "C"
+    D = "D"
+    E = "E"
+    UNKNOWN = "UNKNOWN"
+    NOT_APPLICABLE = "NOT-APPLICABLE"
+
+
+class Nutrition:
+    saturated_fat: str
+    fat: str
+    salt: str
+    sugars: str
+
+
+# Category
+@strawberry.type
+class _CategoryAttr(PydanticBaseModel):
+    name: str
 
 @strawberry.type
-class _ModelAttr(PydanticBaseModel):
-    example: str
+class CategoryBase(BaseModel[product_service_models.CategoryBase], _CategoryAttr):
+    __grpc_model__ = product_service_models.CategoryBase
+
+@strawberry.type
+class Category(BaseModel[product_service_models.Category], _CategoryAttr, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    parents: list["Category"]
+
+
+# Product
+@strawberry.type
+class _ProductAttr(PydanticBaseModel):
+    ean: str
+    name: str
+    generic_name: str
+    nutrition: Nutrition
+    nutri_score: NutriScore
+    ingredients: str
+    quantity: str
+    unit: str
+    keywords: list[str]
+    brands: list[int]  # fk from another service
+    supermakets: list[int]  # fk from another service
+    images: dict[str, str]
+    nutriments: dict[str, str]
 
 
 @strawberry.input
-class ModelBase(BaseModel[product_service_models.ModelBase], _ModelAttr):
-    __grpc_model__ = product_service_models.ModelBase
+class ProductBase(BaseModel[product_service_models.ProductBase], _ProductAttr):
+    __grpc_model__ = product_service_models.ProductBase
 
 
 @strawberry.type
-class Model(BaseModel[product_service_models.Model], _ModelAttr, table=True):
+class Product(BaseModel[product_service_models.Product], _ProductAttr, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
-    __grpc_model__ = product_service_models.Model
+    __grpc_model__ = product_service_models.Product
